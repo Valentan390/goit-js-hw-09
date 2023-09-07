a8694cd74d367b3f263a52b42e9c618ca1993b8f
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-09-07 14:15:10 +0000

    Deploying to gh-pages from @ Valentan390/goit-js-hw-09@3535e1c529fc7d850601a407f7e09ff40b9971dc 🚀

---
+++ 01-color-switcher.c5d8b88d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let d=null;r.setAttribute("disabled","");e.addEventListener("click",(()=>{e.setAttribute("disabled",""),r.removeAttribute("disabled"),d=setInterval((()=>{let e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=e}),1e3)})),r.addEventListener("click",(()=>{e.removeAttribute("disabled"),r.setAttribute("disabled",""),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.c5d8b88d.js.map
